c7a1b5b01ed4c12acdd66830614e939a2306fb15
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-06-30 23:22:24 -1200

    !st July commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+    function Header(){
+      return(
+       <header>
+                <nav className="nav">
+                                         <img src="./logo192.png" alt="react" className ="logo"/>
+                                         <ul className ="Nav-items">
+                                          <li>Pricing</li>
+                                          <li>Contacts</li>
+                                          <li>Blog</li>
+                                         </ul>
+                                          </nav>
+                
+            </header>
+    )
+  }
+function Body(){
+  return(<div className="main">
+                <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+                <li>
+                  honestly i am just hungry for the chance to make something of myself
+                </li>
+            </ol></div>
+  )
 }
 
-export default App;
+function Footer(){
+  return(
+     <footer className ="footer">
+                <small>© 2023 Joshua Lamptey Development. All rights reserved.</small>
+            </footer>
+  )
+}
+
+function Page(){
+  return(
+    <body>
+      <Header />
+         <Body />
+           <Footer />
+    </body>
+  )
+}
+
+export default Page;
